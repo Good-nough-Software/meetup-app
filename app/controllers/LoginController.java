722664bd8ae3140a3c45072d11861b8aace182e6
Lucas Buccilli <lpbuccil@mtu.edu>
2018-10-20 16:47:22 -0400

    Reformatted login page to use less html and use more of the Play framework. I believe this will help us late on with post requests

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -1,11 +1,17 @@
 package controllers;
 
+import com.google.inject.Inject;
+import models.loginForm;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+
+
 /**
  * @author: Lucas Buccilli
- * Class: CS1131
+ * Class: CS3141
  * Date: 10/15/18
  * Time: 2:21 PM
  * Project Name: meetup-app
@@ -13,8 +19,25 @@ import play.mvc.Result;
  */
 public class LoginController extends Controller {
 
-    public Result index() {
-        return ok(views.html.Login.render());
+    @Inject FormFactory formFactory;
+
+
+    public Result renderViewLogin(){
+        Form<loginForm> userForm = formFactory.form(loginForm.class);
+        return ok(views.html.viewLogin.render(userForm));
     }
+
+    public Result login() {
+        Form<loginForm> userForm = formFactory.form(loginForm.class);
+
+        loginForm loginForm = new loginForm("me@test.com", "mypassword");
+
+        Form<loginForm> form = formFactory.form(loginForm.class);
+
+        Form<loginForm> filledForm = form.fill(loginForm);
+
+        return ok(views.html.viewLogin.render(filledForm));
+    }
+
 
 }
