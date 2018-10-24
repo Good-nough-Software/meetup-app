1b9136e198afc262a51bafdb4beb3e432f399368
Jerry Marek <jmarek@mtu.edu>
2018-10-24 14:44:21 -0500

    Make database-setup work... for now...

--- python/database-setup.sql
+++ python/database-setup.sql
@@ -2,38 +2,37 @@
 
 CREATE DATABASE IF NOT EXISTS meetup;
 
-CREATE USER 'meet'@'%';
+CREATE USER IF NOT EXISTS 'meet'@'%';
 GRANT ALL ON meetup.* to 'meet'@'%';
 -- GRANT SELECT ON meetup.* to 'meet'@'%';
 
 USE meetup;
+CREATE TABLE locations (
+       id      INT PRIMARY KEY,
+       country VARCHAR(20),
+       state   VARCHAR(2),
+       city    VARCHAR(32),
+       zip     VARCHAR(9),
+       address VARCHAR(64)
+);
 
 CREATE TABLE users (
-       id       UNSIGNED INT,
+       id       INT,
        username VARCHAR(32),
        password CHAR(40), -- fixed size of hash size
        usertype ENUM('admin', 'user', 'organizer'),
        email    VARCHAR(32),
        name     VARCHAR(32),
        phone    CHAR(11),
-       location UNSIGNED INT,
-       FOREIGN KEY (location) REFERENCES location (id),
+       location INT,
+       FOREIGN KEY (location) REFERENCES locations (id),
        PRIMARY KEY (id, username, email)
 );
 
-CREATE TABLE locations (
-       id      UNSIGNED INT PRIMARY KEY,
-       country VARCHAR(20),
-       state   VARCHAR(2),
-       city    VARCHAR(32),
-       zip     VARCHAR(9),
-       address VARCHAR(64),
-);
-
 CREATE TABLE relations (
-       id         UNSIGNED INT PRIMARY KEY,
-       userid     UNSIGNED INT,
-       locationid UNSIGNED INT,
+       id         INT PRIMARY KEY,
+       userid     INT,
+       locationid INT,
        FOREIGN KEY (userid) REFERENCES users (id),
        FOREIGN KEY (locationid) REFERENCES locations (id)
 );
