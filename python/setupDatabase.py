844d0dc5d21600c93bafb65e5e82003cc99b67ae
Jerry Marek <jmarek@mtu.edu>
2018-10-29 14:38:38 -0500

    Enforce foreign key integrity

--- python/setupDatabase.py
+++ python/setupDatabase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# author Jerry Marek
+# Author Jerry Marek
 
 import hashlib
 import pymysql as mariadb
@@ -19,27 +19,40 @@ def init_good_state(cursor):
     # user[id int auto_inc, username varchar(32), password char(40) usertype enum, 
     #       email varchar(32), name varchar(32), phone char(11), location int]
     password = hashlib.sha1(b'password').hexdigest()
-    users = {"John": ["jsmith", password, "user", "jsmith@gmail.com", "John Smith",
-                        "12345678910"],
-            "Jane": ["jnsmith", password, "user", "cde@gmail.com", "Jane Smith",
-                        "12311234567"]
+    users = {"John": ("jsmith", password, "user", "jsmith@gmail.com", "John Smith",
+                        "12345678910"),
+            "Jane": ("jnsmith", password, "user", "cde@gmail.com", "Jane Smith",
+                        "12311234567")
             };
     # locations [id int auto_inc, country varchar(20), state varchar(2), city varchar(2),
     #               zip varchar(9), address varchar(64)]
-    locations = {"Wads": ["United States", "Mi", "Houghton", "49931",
-                                "1701 Townsend Drive"]
+    locations = {"Wads": ("United States", "Mi", "Houghton", "49931",
+                                "1701 Townsend Drive")
                 };
     # relations [id int auto_inc, userid fk users:is, locationid fk location:id]
     #
-    relations = {"r1": [14, 5],
-                    "r2": [15, 5]
-                };
 
     for user in users:
         cursor.execute("INSERT INTO users (username, password, usertype, email, name, phone) values ('{}', '{}', '{}', '{}', '{}', '{}');".format(*users[user]))
 
     for location in locations:
         cursor.execute("INSERT INTO locations (country, state, city, zip, address) values ('{}', '{}', '{}', '{}', '{}')".format(*locations[location]))
+
+    userids = ()
+    locationid=0
+
+    cursor.execute("SELECT id from users")
+    a = cursor.fetchall()
+    userids = userids + a[0]
+    userids = userids + a[1]
+
+    cursor.execute("SELECT id from locations")
+    for id in cursor:
+        locationid = id[0]
+
+    relations = {"r1": (userids[0], locationid),
+                    "r2": (userids[1], locationid)
+                };
 
     for relation in relations:
         cursor.execute("INSERT INTO relations (userid, locationid) values ('{}', '{}')".format(*relations[relation]))
