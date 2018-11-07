3c7e1e57783d10fbce89b7aed27a73c213dc3abd
Thomas Ingram <taingram@mtu.edu>
2018-11-07 12:04:39 -0500

    Added search model for getting search query

---
+++ app/models/Search.java
@@ -0,0 +1,19 @@
+package models;
+
+import play.data.validation.Constraints;
+
+/**
+ * This class is for getting search querys from
+ */
+public class Search {
+
+  protected String search;
+
+  public void setSearch(String search) {
+    this.search = search;
+  }
+
+  public String getSearch() {
+    return this.search;
+  }
+}
