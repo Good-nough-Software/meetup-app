868f2dfcdadb833a9bac81f5c843e6e450f757df
Thomas Ingram <taingram@mtu.edu>
2018-11-09 11:51:56 -0500

    Simple search working against database

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -37,64 +37,27 @@ public class HomeController extends Controller {
     public Result results() {
         Form<Search> resultsForm = formFactory.form(Search.class).bindFromRequest();
         Search search = resultsForm.get();
-        String query = search.getSearch();
-        if (query == null) {
-            return index();
-        }
-
-        // Test Data
-        List<Location> locations = new ArrayList<Location>();
-        Location l = new Location();
-        l.country = "United States";
-        l.city = "Houghton";
-        l.zip = "49931";
-        l.state = "MI";
-        l.address = "65 Isle Royale St";
-        locations.add(l);
-        Location l2 = new Location();
-        l2.country = "United States";
-        l2.city = "Brighton";
-        l2.zip = "2391";
-        l2.state = "MI";
-        l2.address = "2391 Pepperidge Trl";
-        locations.add(l2);
-
-        // Pull from databases instead
-        // if (query.matches("[\s]")) {
-        //     String[] tokens = query.split("\\s+");
-        // } else {
-        //     String[] tokens = {query};
-        // }
-
-        // // Very simple weighted search
-        // HashMap<Location,Integer> weight = new HashMap<Location,Integer>();
-        // for (String tok: tokens) {
-        //     String sqlstr = "SELECT id FROM locations WHERE country LIKE " + tok;
-        //     SqlQuery sqlquery = Ebean.createSqlQuery(sqlstr);
-
-        //     sqlstr = "SELECT id FROM locations WHERE city LIKE " + tok;
-        //     sqlquery = Ebean.createSqlQuery(sqlstr);
-
-        //     sqlstr = "SELECT id FROM locations WHERE state LIKE " + tok;
-        //     sqlquery = Ebean.createSqlQuery(sqlstr);
-
-        //     sqlstr = "SELECT id FROM locations WHERE zip LIKE " + tok;
-        //     sqlquery = Ebean.createSqlQuery(sqlstr);
-
-        //     sqlstr = "SELECT id FROM locations WHERE address LIKE " + tok;
-        //     sqlquery = Ebean.createSqlQuery(sqlstr);
-        // }
-
-        // String dbquery = "SELECT id FROM locations WHERE LIKE ";
-
-        List<Location> matches = new ArrayList<Location>();
-        for (Location location: locations) {
-            String address = location.toString();
-            if (address.contains(query))
-                matches.add(location);
-        }
+        String query = search.getSearch().toLowerCase();
 
         Form<Search> form = formFactory.form(Search.class);
+        if (query.isEmpty()) {
+            List<Location> empty = new ArrayList<>();
+            return ok(views.html.results.render(form, empty, query));
+        }
+
+        List<Location> locations = Location.find.all();
+
+        String[] tokens = query.split("\\s+");
+        List<Location> matches = new ArrayList<Location>();
+        for (String tok : tokens) {
+            // Replace with queries to DB
+            for (Location location: locations) {
+                String address = location.toString();
+                if (address.toLowerCase().contains(tok))
+                    matches.add(location);
+            }
+        }
+
         return ok(views.html.results.render(form, matches, query));
     }
 }
--- app/models/Location.java
+++ app/models/Location.java
@@ -1,13 +1,34 @@
 package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import io.ebean.*;
+
 /**
  * Location object to pull data from database
  */
-public class Location {
+@Entity
+@Table(name="locations")
+public class Location extends Model {
+
+  @Id
+  public int id;
+
   public String country;
   public String state;
   public String city;
-  public String address;
   public String zip;
+  public String address;
+
+  public static final Finder<Long, Location> find = new Finder<>(Location.class);
+
+  public boolean equals(Location other) {
+    if (this.id == other.id) {
+      return true;
+    }
+    return false;
+  }
 
   public String toString() {
     return address + "\n" + city + " " + state + " " + zip + "\n" + country;
